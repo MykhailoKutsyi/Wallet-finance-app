d98b10279ef17cb8f6095e6de33c38e8fd89c355
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-07-28 19:19:47 +0300

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { GlobalStyle } from './components/style/GlobalStyle';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        Wallet
+      </div>
+    </React.Fragment>
+  );
+}
