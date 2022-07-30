49a2ec76b336d7a7d6fac53d8ce80446b3f88d40
Akim Grakovskiy <77636476+Akim3351@users.noreply.github.com>
2022-07-30 12:44:05 +0300

    AppBar created

---
+++ src/components/AppBar/AppBar.styled.js
@@ -0,0 +1,73 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+height: 60px;
+padding: 0px 20px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+background-color: fff;
+
+    @media (min-width: 768px) {
+    height: 80px;
+    }
+`;
+
+export const RightSideGroup = styled.div`
+padding: 15px 0px;
+display: flex;
+align-items: center;
+`;
+
+export const UserName = styled.p`
+margin: 0px 10px 0px 0px;
+padding: 0px;
+font: Circe;
+font-weight: 400;
+font-size: 18px;
+line-height: 1.5;
+text-align: right;
+color: #BDBDBD;
+
+    @media (min-width: 768px) {
+    padding: 0px 10px 0px 0px;
+    border-right: 1px solid #BDBDBD;
+    }
+`;
+
+export const ExitBtn = styled.button`
+border: none;
+background-color: #fff;
+display: flex;
+align-items: center;
+outline: none;
+    &:hover{
+        cursor: pointer;
+    }
+`;
+
+export const BtnIconWrapper = styled.svg`
+    height: 18px;
+    width: 18px;
+
+    &:hover{
+    cursor: pointer;
+}`;
+
+
+export const BtnSpan = styled.span`
+margin: 0px 0px 0px 10px;
+padding: 0px;
+font: Circe;
+font-weight: 400;
+font-size: 18px;
+line-height: 1.5;
+text-align: right;
+color: #BDBDBD;
+
+    @media (max-width: 768px) {
+    display: none;
+    }
+
+`;
