2bf47837c5d6862f2cf34b93bd5b1c8aedbbcc73
Mykhailo Kutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-07-31 13:45:02 +0300

    add librarys and update loader

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,34 +1,35 @@
 import Logo from '../Logo/Logo.jsx';
-import sprite from "../../images/sprite.svg";
+import sprite from 'images/sprite.svg';
 import { useSelector } from 'react-redux';
-import {getUserName} from '../../redux/auth/auth-selectors';
-import { Header, RightSideGroup, UserName, ExitBtn, BtnIconWrapper, BtnSpan } from './AppBar.styled';
-
+import { getUserName } from '../../redux/auth/auth-selectors';
+import {
+  Header,
+  RightSideGroup,
+  UserName,
+  ExitBtn,
+  BtnIconWrapper,
+  BtnSpan,
+} from './AppBar.styled';
 
 const AppBar = () => {
-
-    const name = useSelector(getUserName);
-                
-    return (
-            <Header>
-                <Logo />
-                <RightSideGroup>
-                <UserName>
-                    {name}
-                </UserName>
-                <ExitBtn
-                    onClick={() => console.log("click!!!")} // заглушка, замість цього буде діспатч, який змінюватиме у store стан модалки
-                >
-                    <BtnIconWrapper >
-                            <use href={`${sprite}#icon-exit`} />
-                    </BtnIconWrapper>
-                    <BtnSpan>
-                        Exit
-                    </BtnSpan>
-                    </ExitBtn>
-                </RightSideGroup>
-            </Header >
-    )
+  const name = useSelector(getUserName);
+  console.log(name);
+  return (
+    <Header>
+      <Logo />
+      <RightSideGroup>
+        <UserName>{name}</UserName>
+        <ExitBtn
+          onClick={() => console.log('click!!!')} // заглушка, замість цього буде діспатч, який змінюватиме у store стан модалки
+        >
+          <BtnIconWrapper>
+            <use href={`${sprite}#icon-exit`} />
+          </BtnIconWrapper>
+          <BtnSpan>Exit</BtnSpan>
+        </ExitBtn>
+      </RightSideGroup>
+    </Header>
+  );
 };
 
 export default AppBar;
