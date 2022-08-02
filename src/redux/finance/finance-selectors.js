a7543741035a0c130efab02fe3c807ce6f593b26
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-08-02 20:20:44 +0300

    feature/balance

--- src/redux/finance/finance-selectors.js
+++ src/redux/finance/finance-selectors.js
@@ -2,11 +2,13 @@ const getIsModalAddTransaction = state =>
   state.global.isModalAddTransactionOpen;
 const getIsModalLogout = state => state.global.isModalLogoutOpen;
 const getIsLoading = state => state.global.isLoading;
+const getTotalBalance = state => state.finance.totalBalance;
 
 const globalSelectors = {
   getIsModalAddTransaction,
   getIsModalLogout,
   getIsLoading,
+  getTotalBalance,
 };
 
 export default globalSelectors;
