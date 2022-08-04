a7360b3af15478a02b6e01033e43799cb16af440
pavel11988 <kossoua88@gmail.com>
2022-08-05 01:44:00 +0300

    fix selectors

--- src/redux/finance/finance-selectors.js
+++ src/redux/finance/finance-selectors.js
@@ -1,7 +1,9 @@
-getCurrentTransactions = state => state.finance.data;
+const getCurrentTransactions = state => state.finance.data;
+const getTotalBalance = state => state.finance.totalBalance;
 
 const financeSelectors = {
   getCurrentTransactions,
+  getTotalBalance,
 };
 
 export default financeSelectors;
