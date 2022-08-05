030f0b3b86506d8bf2abad44e3f12c608961abd3
Akim Grakovskiy <77636476+Akim3351@users.noreply.github.com>
2022-08-06 00:18:05 +0300

    Update DiagramTab.jsx
    Є приблизна логіка обробки відомостей про транзакції, потрібно налаштовувати запит на бекенд

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -11,11 +11,21 @@ import { Wrapper, SubWrapper } from "./DiagramTab.styled";
 const DiagramTab = () => {
   const currentBalance = useSelector(globalSelectors.getTotalBalance);
 
+  const userTransactions = useSelector(globalSelectors.getTransactions) || [];
+
   const [datesForFilter, setDatesForFilter] = useState({
     firstDate: null,
     secondDate: null,
   });
-    
+
+  const filteredTransactionsByDate = userTransactions.filter(item => ((item.date >= datesForFilter.firstDate) && (item.date <= datesForFilter.secondDate)));
+
+  console.log(filteredTransactionsByDate);
+
+  const filteredExpenses = filteredTransactionsByDate.filter(item => item.type === 'expense');
+
+  const filteredIncomes = filteredTransactionsByDate.filter(item => item.type === 'income');
+
   const colorsArray = [
     '#fed057',
     '#ffd8d0',
@@ -39,35 +49,67 @@ const DiagramTab = () => {
     'Leisure',
     'Other expenses'
   ];
+  
+  const dataForTable = expenseArray.map(item => {
+    const totalExpenseAmount = filteredExpenses.map(exp => {
+      if (exp.category === item) {
+        return item;
+      };
+    })
+      .map(item => item.amount)
+      .reduce((acc, value) => {
+        return acc + value
+      }, 0);
+    const tableRowItem = {
+      color: colorsArray[expenseArray.indexOf(item)],
+      expense: item,
+      value: totalExpenseAmount,
+    }
+    return tableRowItem;
+  });
+
+  // const dataForTable = [ 
+  //   {color: colorsArray[0], expense: expenseArray[0], value: 8700.00},
+  //   {color: colorsArray[1], expense: expenseArray[1], value: 3800.74},
+  //   {color: colorsArray[2], expense: expenseArray[2], value: 1500.00},
+  //   {color: colorsArray[3], expense: expenseArray[3], value: 800.00},
+  //   {color: colorsArray[4], expense: expenseArray[4], value: 2208.50},
+  //   {color: colorsArray[5], expense: expenseArray[5], value: 300.00},
+  //   {color: colorsArray[6], expense: expenseArray[6], value: 3400.00},
+  //   {color: colorsArray[7], expense: expenseArray[7], value: 1230.00},
+  //   {color: colorsArray[8], expense: expenseArray[8], value: 610.00}
+  //   ]; 
 
   const dataForChart = {
     labels: expenseArray,
     datasets: [
       {
         label: '# of Votes',
-        data: [8700.00, 3800.74, 1500.00, 800.00, 2208.50, 300.00, 3400.00, 1230.00, 610.00],
+        data: dataForTable.map(item => item.value),
+        // data: [8700.00, 3800.74, 1500.00, 800.00, 2208.50, 300.00, 3400.00, 1230.00, 610.00],
         backgroundColor: colorsArray,
         borderColor: colorsArray,
         borderWidth: 1,
       },
     ],
-    }; 
+  }; 
   
-  const dataForTable = [ 
-    {color: colorsArray[0], expense: expenseArray[0], value: 8700.00},
-    {color: colorsArray[1], expense: expenseArray[1], value: 3800.74},
-    {color: colorsArray[2], expense: expenseArray[2], value: 1500.00},
-    {color: colorsArray[3], expense: expenseArray[3], value: 800.00},
-    {color: colorsArray[4], expense: expenseArray[4], value: 2208.50},
-    {color: colorsArray[5], expense: expenseArray[5], value: 300.00},
-    {color: colorsArray[6], expense: expenseArray[6], value: 3400.00},
-    {color: colorsArray[7], expense: expenseArray[7], value: 1230.00},
-    {color: colorsArray[8], expense: expenseArray[8], value: 610.00}
-    ]; 
-  const expenses = 22549.24; 
+  // const expenses = 22549.24;
 
-  const income = 27350.00; 
+  const expenses = filteredExpenses
+    .map(item => item.amount)
+    .reduce((acc, value) => {
+      return acc + value
+    }, 0);
 
+  // const income = 27350.00;
+
+  const income = filteredIncomes
+    .map(item => item.amount)
+    .reduce((acc, value) => {
+      return acc + value
+    }, 0);
+  
   const onDateInput = (event) => {
     const { firstDate, secondDate } = datesForFilter;
 
