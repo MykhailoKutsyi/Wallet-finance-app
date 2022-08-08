226f2c34269a25c71c11cca6c70956b67d7be9aa
MykhailoKutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-08-08 15:24:55 +0300

    update Statistics

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import Balance from "../../components/Balance/Balance.jsx";
-import Currency from "../../components/Currency/Currency.jsx";
-import Navigation from "../../components/Navigation/Navigation.jsx";
-import DiagramTab from "../../components/DiagramTab/DiagramTab.jsx";
-import { CurrencyWrapper, HomeInfo, NavBalWrapper,  } from "../Dashboard/Dashboard.styled";
-import { AsideWrapper } from "./Statistics.styled.js";
+import { useState } from 'react';
+// import { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from "react-redux";
+import Balance from 'components/Balance/Balance.jsx';
+import Currency from 'components/Currency/Currency.jsx';
+import Navigation from 'components/Navigation/Navigation.jsx';
+import DiagramTab from 'components/DiagramTab/DiagramTab.jsx';
+import {
+  CurrencyWrapper,
+  HomeInfo,
+  NavBalWrapper,
+} from '../Dashboard/Dashboard.styled';
+import { AsideWrapper } from './Statistics.styled.js';
 
 export default function Statistics() {
-
   const [viewCurrency, setViewCurrency] = useState(false);
 
   window.addEventListener('resize', function () {
@@ -17,24 +21,23 @@ export default function Statistics() {
     }
   });
 
-  useEffect(() => {}, [viewCurrency]);
-
+  // useEffect(() => {}, [viewCurrency]);
 
   return (
     <AsideWrapper>
-        <HomeInfo>
-            <NavBalWrapper>
-              <Navigation
-                setViewCurrency={setViewCurrency}
-                viewCurrency={viewCurrency}
-              />
-              <Balance />
-            </NavBalWrapper>
-            <CurrencyWrapper>
-              <Currency />
-            </CurrencyWrapper>
-        </HomeInfo>
-        <DiagramTab />
+      <HomeInfo>
+        <NavBalWrapper>
+          <Navigation
+            setViewCurrency={setViewCurrency}
+            viewCurrency={viewCurrency}
+          />
+          <Balance />
+        </NavBalWrapper>
+        <CurrencyWrapper>
+          <Currency />
+        </CurrencyWrapper>
+      </HomeInfo>
+      <DiagramTab />
     </AsideWrapper>
   );
-};
+}
