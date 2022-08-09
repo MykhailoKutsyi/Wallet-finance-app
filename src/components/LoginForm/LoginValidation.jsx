57a6496ed14df96d1b4e79687ebe0d66085b7f07
Andriy-Lesyk <lesikandrey.uh@gmail.com>
2022-08-10 00:31:01 +0300

    valid

--- src/components/LoginForm/LoginValidation.jsx
+++ src/components/LoginForm/LoginValidation.jsx
@@ -2,8 +2,23 @@ import * as Yup from 'yup';
 
 function LoginValidation() {
     const validate = Yup.object({
-    email: Yup.string().email('Email is invalid').required('Email is required'),
-    password: Yup.string().min(6).max(16).required('Password is required'),
+    email: Yup.string()
+      .email('Email is invalid')
+      .min(10)
+      .max(63)
+      .required('Email is required')
+      .test({
+        message: 'before @ at least 2 characters',
+        test: value => value[0]&&value[1]!=='@',
+      })
+      .test({
+        message: 'do not type dash at the start ',
+        test: value => value[0]!=='-',
+      }),
+    password: Yup.string().min(6).max(16).required('Password is required').matches(
+      /^(?=.*[a-z])(?=.*\d)[a-z\d@$!%*#?&]{6,}$/,
+      "Must Contain min 6 Char, One Letter, One Number"
+    ),
   });
   return validate
 }
