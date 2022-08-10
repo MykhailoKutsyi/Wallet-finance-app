01b7bdd6a6483ed0c5089eb10489a1bbc3140350
Andriy-Lesyk <lesikandrey.uh@gmail.com>
2022-08-10 16:51:22 +0300

    val

--- src/components/LoginForm/LoginValidation.jsx
+++ src/components/LoginForm/LoginValidation.jsx
@@ -17,7 +17,7 @@ function LoginValidation() {
       }).matches(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/, "Email is invalid"
 ),
     password: Yup.string().min(6).max(16).required('Password is required').matches(
-      /^(?=.*[a-z])(?=.*\d)[a-z\d@$!%*#?&]{0,}$/,
+      /^(?=.*[a-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{0,}$/,
       "Must Contain at least one letter and one number"
     ),
   });
